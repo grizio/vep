ae1e387f30c34886a9885714b40131efbc235266
Gaëtan Rizio <gaetan@rizio.fr>
2016-04-03 11:55:39 +0200

    Updates gulp file for production build and improve dev build. Updates html files for build needs.

--- vepweb/gulpfile.js
+++ vepweb/gulpfile.js
@@ -1,39 +1,88 @@
 var gulp = require("gulp");
+var gutil = require('gulp-util');
 var watch = require('gulp-watch');
 var plumber = require('gulp-plumber');
 var babel = require("gulp-babel");
 var webserver = require('gulp-webserver');
+var uglify = require('gulp-uglify');
+var minifyInline = require('gulp-minify-inline');
+var htmlmin = require('gulp-htmlmin');
+var inlinesource = require('gulp-inline-source');
 
-gulp.task("compileJs", function () {
-  return gulp.src("src/**/*.es6")
+/// Constants
+var tasks = {
+  devHTML: "devHTML",
+  devJS: "devJS",
+  dev: "dev",
+  buildHTML: "buildHTML",
+  buildJS: "buildJS",
+  build: "build",
+  run: "run"
+};
+
+var paths = {
+  html: "src/**/*.html",
+  js: "src/**/*.es6",
+  dist: "dist",
+  distHTML: "dist/** / *.html"
+};
+
+/// Helpers
+function handleError(e) {
+  gutil.log(gutil.colors.red(e));
+  this.emit("end");
+}
+
+/// Dev tasks
+gulp.task(tasks.devJS, function () {
+  return gulp.src(paths.js)
     .pipe(babel())
-    .pipe(gulp.dest("dist"));
+    .on('error', handleError)
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task("js", ["compileJs"], function () {
-  return gulp.src("src")
-    .pipe(watch("src/**/*.es6"))
-    .pipe(plumber())
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+gulp.task(tasks.devHTML, function () {
+  return gulp.src(paths.html)
+    .on('error', handleError)
+    .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task("compileHtml", function () {
-  return gulp.src("src/**/*.html")
-    .pipe(gulp.dest("dist"));
-});
+gulp.task(tasks.dev, [tasks.devJS, tasks.devHTML], function () {
+  gulp.watch(paths.js, [tasks.devJS]);
+  gulp.watch(paths.html, [tasks.devHTML]);
 
-gulp.task("html", ["compileHtml"], function () {
-  return gulp.src(".")
-    .pipe(watch("src/**/*.html"))
-    .pipe(plumber())
-    .pipe(gulp.dest("dist"));
-});
-
-gulp.task("webserver", function () {
   gulp.src(".")
     .pipe(webserver({
       livereload: true,
+      fallback: "index.html"
+    }));
+});
+
+/// Prod tasks
+gulp.task(tasks.buildHTML, function () {
+  return gulp.src(paths.html)
+    .pipe(minifyInline())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task(tasks.buildJS, function () {
+  return gulp.src(paths.js)
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task(tasks.build, [tasks.buildHTML, tasks.buildJS], function () {
+  return gulp.src(paths.distHTML)
+    .pipe(inlinesource())
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task(tasks.run, [tasks.build], function () {
+  gulp.src(".")
+    .pipe(webserver({
+      livereload: false,
       fallback: "index.html"
     }));
 });
