e4397b141dc9794859f3cae9a2db036c91831516
Gaëtan Rizio <gaetan@rizio.fr>
2017-10-19 00:18:56 +0200

    Try to detect search bots to remove javascript rendering
    Google bot execute the javascript, so the initial render is lost.
    But Google bot does not wait until data are visible.
    
    This commit do the following:
    
    * Detect bots
    * If bots are detected, remove javascript calls

--- src/main/scala/vep/app/seo/views/ViewsRouter.scala
+++ src/main/scala/vep/app/seo/views/ViewsRouter.scala
@@ -30,39 +30,57 @@ class ViewsRouter(
   }
 
   def home: Route = pathEndOrSingleSlash {
-    found(pageService.find("home")) { page =>
-      OkView(PageView.view(page))
-    }
-  }
-
-  def page: Route = publicGet("page" / Segment) { canonical =>
-    found(pageService.find(canonical)) { page =>
-      OkView(PageView.view(page))
-    }
-  }
-
-  def show: Route = publicGet("production" / "companies" / Segment / "shows" / "page" / Segment) { (companyId, showId) =>
-    found(companyService.find(companyId)) { company =>
-      found(showService.findFromCompanyWithDependencies(company, showId)) { show =>
-        OkView(ShowView.view(show))
+    optionalHeaderValueByName("User-Agent") { userAgent =>
+      found(pageService.find("home")) { page =>
+        OkView(PageView.view(page), userAgent)
       }
     }
   }
 
-  def play: Route = publicGet("production" / "companies" / Segment / "shows" / Segment / "plays" / "page" / Segment) { (companyId, showId, playId) =>
-    found(companyService.find(companyId)) { company =>
-      found(showService.findFromCompany(company, showId)) { show =>
-        found(playService.findFromShowWithDependencies(show, playId)) { play =>
-          OkView(PlayView.view(play, playService.findAllFromShow(show)))
+  def page: Route = publicGet("page" / Segment) { canonical =>
+    optionalHeaderValueByName("User-Agent") { userAgent =>
+      found(pageService.find(canonical)) { page =>
+        OkView(PageView.view(page), userAgent)
+      }
+    }
+  }
+
+  def show: Route = publicGet("production" / "companies" / Segment / "shows" / "page" / Segment) { (companyId, showId) =>
+    optionalHeaderValueByName("User-Agent") { userAgent =>
+      found(companyService.find(companyId)) { company =>
+        found(showService.findFromCompanyWithDependencies(company, showId)) { show =>
+          OkView(ShowView.view(show), userAgent)
         }
       }
     }
   }
 
-  private def OkView(content: String): HttpResponse = {
+  def play: Route = publicGet("production" / "companies" / Segment / "shows" / Segment / "plays" / "page" / Segment) { (companyId, showId, playId) =>
+    optionalHeaderValueByName("User-Agent") { userAgent =>
+      found(companyService.find(companyId)) { company =>
+        found(showService.findFromCompany(company, showId)) { show =>
+          found(playService.findFromShowWithDependencies(show, playId)) { play =>
+            OkView(PlayView.view(play, playService.findAllFromShow(show)), userAgent)
+          }
+        }
+      }
+    }
+  }
+
+  private def OkView(content: String, userAgent: Option[String]): HttpResponse = {
+    val normalizedContent = if (userAgent.exists(isBot)) {
+      content.replaceAll("<script(.*)</script>", "")
+    } else {
+      content
+    }
     Ok(
-      entity = content,
+      entity = normalizedContent,
       headers = immutable.Seq(`Content-Type`(ContentTypes.`text/html(UTF-8)`))
     )
+  }
+
+  private def isBot(userAgent: String): Boolean = {
+    val userAgentBots = Seq("bot", "crawl", "slurp", "spider", "mediapartners")
+    userAgentBots.exists(userAgentBot => userAgent.toLowerCase().contains(userAgentBot))
   }
 }
